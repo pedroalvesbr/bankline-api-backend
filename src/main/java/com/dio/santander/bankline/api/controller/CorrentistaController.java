f884633c5de56ea3233caa6510781d4155d89447
Pedro Alves <pedro.alves1705@gmail.com>
2022-05-08 23:04:27 -0300

    versao final do nosso projeto ! :100:
    funcionando a API, registrando alteração entre movimentações e correntistas

--- src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
+++ src/main/java/com/dio/santander/bankline/api/controller/CorrentistaController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dio.santander.bankline.api.dto.NovoCorrentista;
 import com.dio.santander.bankline.api.model.Correntista;
 import com.dio.santander.bankline.api.repository.CorrentistaRepository;
+import com.dio.santander.bankline.api.services.CorrentistaService;
 
 //esse cara vai saber tudo da camada do nosso webService para gerar os recursos
 //incluir, remover, alterar os correntistas
@@ -21,6 +23,10 @@ public class CorrentistaController {
 	
 	@Autowired
 	private CorrentistaRepository repository;
+	
+	@Autowired
+	private CorrentistaService service;
+	
 	@GetMapping
 	public List<Correntista> findAll(){		
 		return repository.findAll();
@@ -28,8 +34,8 @@ public class CorrentistaController {
 	
 	//adiciona novos correntistas
 	@PostMapping
-	public void save(@RequestBody Correntista correntista) {
-							
+	public void save(@RequestBody NovoCorrentista correntista) {
+		service.save(correntista);						
 
 	}
 }
---
+++ src/main/java/com/dio/santander/bankline/api/controller/MovimentacaoController.java
@@ -0,0 +1,42 @@
+package com.dio.santander.bankline.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dio.santander.bankline.api.repository.MovimentacaoRepository;
+import com.dio.santander.bankline.api.services.MovimentacaoService;
+import com.dio.santander.bankline.api.dto.NovaMovimentacao;
+import com.dio.santander.bankline.api.model.Movimentacao;
+
+//todo o controle de nossas movimentações bancarias
+
+@RestController
+@RequestMapping("/movimentacoes")
+public class MovimentacaoController {
+	
+	@Autowired
+	private MovimentacaoRepository repository;
+	
+	@Autowired
+	private MovimentacaoService service;
+	
+	//para listar a nossa movimentação
+	@GetMapping
+	public List<Movimentacao> findAll(){
+		return repository.findAll();
+	}
+	
+	@PostMapping
+	public void save(@RequestBody NovaMovimentacao movimentacao) {
+		service.save(movimentacao);
+	}
+	
+	
+
+}
